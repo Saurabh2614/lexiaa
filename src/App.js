affb101782fd8e32c968e1d6af6b93f8857a0cb3
Akadu <tambesaurabh14@gmail.com>
2025-07-10 09:33:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>lexi</h1>
+      <div className='flex '>
+        <input type='text' placeholder='enter your query'></input>
+        <input type='button' value="submit" />
+      </div>
     </div>
   );
 }
